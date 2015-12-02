c87f85096ab420d21a8c5cbd782b8a0373e8fbad
user1 <user1@4399-0924>
2015-12-02 10:48:16 +0800

    1

--- src/com/cantalou/android/util/FileUtil.java
+++ src/com/cantalou/android/util/FileUtil.java
@@ -1,7 +1,6 @@
 package com.cantalou.android.util;
 
 import java.io.BufferedInputStream;
-import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 
@@ -10,75 +9,77 @@ import android.content.res.AssetManager;
 
 public class FileUtil {
 
-	/**
-	 * 从assets目录复制文件到指定路径
-	 *
-	 * @param context context
-	 * @param srcFileName
-	 *            复制的文件名
-	 * @param targetDir
-	 *            目标目录
-	 * @param targetFileName
-	 *            目标文件名
-	 * @return
-	 */
-	public static boolean copyAssetsFile(Context context, String srcFileName, String targetFilePath) {
-		AssetManager asm = context.getAssets();
-		FileOutputStream fos = null;
-		BufferedInputStream bis = null;
-		try {
-			createDir(targetFilePath);
-			File targetFile = new File(targetFilePath);
-			if (targetFile.exists()) {
-				targetFile.delete();
-			}
+    /**
+     * 从assets目录复制文件到指定路径
+     *
+     * @param context
+     *            context
+     * @param srcFileName
+     *            复制的文件名
+     * @param targetDir
+     *            目标目录
+     * @param targetFileName
+     *            目标文件名
+     *            
+     * @return 复制是否成功
+     */
+    public static boolean copyAssetsFile(Context context, String srcFileName, String targetFilePath) {
+	AssetManager asm = context.getAssets();
+	FileOutputStream fos = null;
+	BufferedInputStream bis = null;
+	try {
+	    createDir(targetFilePath);
+	    File targetFile = new File(targetFilePath);
+	    if (targetFile.exists()) {
+		targetFile.delete();
+	    }
 
-			bis = new BufferedInputStream(asm.open(srcFileName));
-			fos = new FileOutputStream(targetFile);
-			byte[] buffer = new byte[8 * 1024];
-			int len = 0;
-			while ((len = bis.read(buffer)) != -1) {
-				fos.write(buffer, 0, len);
-				fos.flush();
-			}
-			return true;
-		} catch (Exception e) {
-			Log.e(e);
-		} finally {
-			try {
-				if (bis != null) {
-					bis.close();
-				}
-			} catch (Exception e2) {
-			}
-
-			try {
-				if (fos != null) {
-					fos.close();
-				}
-			} catch (Exception e2) {
-			}
+	    bis = new BufferedInputStream(asm.open(srcFileName));
+	    fos = new FileOutputStream(targetFile);
+	    byte[] buffer = new byte[8 * 1024];
+	    int len = 0;
+	    while ((len = bis.read(buffer)) != -1) {
+		fos.write(buffer, 0, len);
+		fos.flush();
+	    }
+	    return true;
+	} catch (Exception e) {
+	    Log.e(e);
+	} finally {
+	    try {
+		if (bis != null) {
+		    bis.close();
 		}
-		return false;
-	}
+	    } catch (Exception e2) {
+	    }
 
-	/**
-	 * 创建文件夹
-	 * 
-	 * @param dir
-	 */
-	public static void createDir(String dir) {
-		createDir(new File(dir));
-	}
-
-	/**
-	 * 创建文件夹
-	 * 
-	 * @param f
-	 */
-	public static void createDir(File f) {
-		if (!f.exists()) {
-			f.mkdirs();
+	    try {
+		if (fos != null) {
+		    fos.close();
 		}
+	    } catch (Exception e2) {
+	    }
 	}
+	return false;
+    }
+
+    /**
+     * 创建文件夹
+     * 
+     * @param dir
+     */
+    public static void createDir(String dir) {
+	createDir(new File(dir));
+    }
+
+    /**
+     * 创建文件夹
+     * 
+     * @param f
+     */
+    public static void createDir(File f) {
+	if (!f.exists()) {
+	    f.mkdirs();
+	}
+    }
 }
